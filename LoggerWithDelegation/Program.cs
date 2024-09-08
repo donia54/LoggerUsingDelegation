eb4b9424e241acfe96ceb39721916c5fb05cbf29
donia <arefdonia491@gmail.com>
2024-09-08 07:03:15 +0300

    Initial commit

---
+++ LoggerWithDelegation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerWithDelegation
+{
+
+    public class Logger
+    {
+        public delegate void LoggerAction(string message);
+        private LoggerAction _Action;
+
+        public Logger(LoggerAction action)
+        {
+            _Action = action;
+        }
+
+        public void Log(string message)
+        {
+            _Action(message);
+        }
+    }
+
+    public class Program
+    {
+       public static void LogToScreen(string message)
+       {
+            Console.WriteLine(message);
+       }
+
+        public static void LogToFile(string message)
+        {
+            string fileName = "log.txt";
+            using (StreamWriter writer = new StreamWriter(fileName, true))
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Logger ScreenLogger = new Logger(LogToScreen);
+            Logger FileLogger = new Logger(LogToScreen);
+
+            ScreenLogger.Log("Screen ");
+            FileLogger.Log("Logged to file");
+
+            Console.ReadKey();
+        }
+    }
+}
